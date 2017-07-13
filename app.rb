1e0d5dab9fd03534c89dc7639b98a8eb905c9ea4
guidovitafinzi <guido.vitafinzi@gmail.com>
2017-07-13 11:45:38 +0100

    Added views

--- app.rb
+++ app.rb
@@ -5,12 +5,6 @@ get '/' do
 	'hello!'
 end
 
-get '/secret' do
-	'secret page'
-end
-
 get '/cat' do
-	"<div style='border: 3px dashed red'>
-	<img src='http://bit.ly/1eze8aE'>
-	</div>"
+	erb(:index)
 end
